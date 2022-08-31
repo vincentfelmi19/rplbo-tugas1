2897235beb38bd2b8167ddc4608d39bc8ddabcc5
vincentfelmi19 <vincentsius.felmi@ti.ukdw.ac.id>
2022-08-31 20:19:51 +0700

    71180332 program java

---
+++ 71180332/n71180332.java
@@ -0,0 +1,11 @@
+public class n71180332 {
+
+    public static void main(String[] args) {
+        int tambah = 0;
+        for (int i = 1; i < 333; i++) {
+            tambah = tambah + i;
+        }
+        System.out.println("Hasilnya = "+tambah);
+        System.out.println("TERIMA KASIH 71180332 VINCENTSIUS FELMI AJANG");
+    }
+}
